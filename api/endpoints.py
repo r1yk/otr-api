2be8180f9d30e35dc569357a1de553f81db1cbea
Roy Kresge <49421368+r1yk@users.noreply.github.com>
2022-12-18 16:36:32 -0500

    remove all auth

--- api/endpoints.py
+++ api/endpoints.py
@@ -1,149 +1,34 @@
+"""API endpoints"""
 from typing import List, Union
 
-from fastapi import APIRouter, Depends, Header, Request, Response
-from fastapi.security import OAuth2PasswordBearer
-from sqlalchemy import and_, nulls_last
+from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
-from lib.auth import JWT, OTRAuth, WEB_APP_SECRET, SECRET_KEY
-from lib.models import Lift, Resort, Trail, UserResort
+from lib.models import Lift, Resort, Trail
 from lib.postgres import get_api_db
 import lib.schemas as schemas
-from webscraper import scrape_resort
 
 
-async def authorize_new_user(
-    request: Request, authorization: str | None = Header(None)
-):
-    """
-    Handle requests to create new users. This can only be done by the web application.
-    Make sure the `Authorization` header is present, and that the token was signed
-    by the web application.
-    """
-    payload = await request.json()
-    secret = f"otr-verify:{payload.get('email')}@{payload.get('password')}"
-    return _authorize(request, authorization, secret, split_on="Bearer ")
-
-
-def authorize(
-    request: Request,
-    authorization: str | None = Header(None),
-    cookie: str | None = Header(None),
-):
-    """Handle requests made on behalf of individual users."""
-    token_header = cookie or authorization
-    split_on = "otr_auth=" if cookie else "Bearer "
-    return _authorize(request, token_header, secret=SECRET_KEY, split_on=split_on)
-
-
-def _authorize(
-    request: Request, bearer_token, secret: str = SECRET_KEY, split_on="otr_auth="
-):
-    """
-    Verify that a given bearer token is both unexpired and signed with a given secret.
-    """
-    if not bearer_token:
-        OTRAuth.return_status(401)
-
-    components = bearer_token.split(split_on)
-    if len(components) == 2:
-        token = components[1]
-        jwt = JWT(secret)
-        message = jwt.decode_token(token)
-        request.state.user_id = message["sub"]
-        return message
-
-    OTRAuth.return_status(401)
-
-
-def get_user_id(request: Request) -> str:
-    return request.state.user_id
-
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
-
-router = APIRouter(dependencies=[Depends(oauth2_scheme), Depends(authorize)])
-
-
-@router.post("/logout")
-async def logout(response: Response):
-    response.headers["Set-Cookie"] = f"otr_auth=null; HttpOnly"
-    return {"access_token": None, "token_type": "bearer"}
+router = APIRouter()
 
 
 @router.get(
     "/resorts", response_model=Union[List[schemas.ResortWithUser], List[schemas.Resort]]
 )
-def get_resorts(
-    user_id: str = Depends(get_user_id), db_session: Session = Depends(get_api_db)
-):
-    if user_id:
-        # Join resorts with user_resorts when a user id is provided.
-        query = (
-            db_session.query(Resort, UserResort.user_id)
-            .join(
-                UserResort,
-                and_(UserResort.resort_id == Resort.id, UserResort.user_id == user_id),
-                isouter=True,
-            )
-            .order_by(nulls_last(UserResort.user_id), Resort.name.asc())
-            .all()
-        )
-        return [
-            schemas.ResortWithUser(
-                **resort_with_user[0].__dict__, user_id=resort_with_user[1]
-            )
-            for resort_with_user in query
-        ]
-
-    # Return all resorts
+def get_resorts(db_session: Session = Depends(get_api_db)):
+    """Return all resorts"""
     return db_session.query(Resort).order_by(Resort.name.asc()).all()
-
-
-@router.post("/resorts/{resort_id}/pin")
-def pin_resort_by_id(
-    resort_id: str,
-    user_id: str = Depends(get_user_id),
-    db_session: Session = Depends(get_api_db),
-):
-    db_session.add(UserResort(user_id=user_id, resort_id=resort_id))
-    db_session.commit()
-    return {}
-
-
-@router.delete("/resorts/{delete_resort_id}/pin")
-def delete_resort_pin_by_id(
-    delete_resort_id: str,
-    user_id: str = Depends(get_user_id),
-    db_session: Session = Depends(get_api_db),
-):
-    user_resort = (
-        db_session.query(UserResort)
-        .filter_by(resort_id=delete_resort_id, user_id=user_id)
-        .one()
-    )
-    db_session.delete(user_resort)
-    db_session.commit()
-    return {}
-
-
-@router.post(
-    "/resorts/{resort_id}/scrape",
-    response_model=schemas.Resort,
-    include_in_schema=False,
-)
-def update_resort_by_id(resort_id: str, db_session: Session = Depends(get_api_db)):
-    scrape_resort(resort_id)
-    return db_session.query(Resort).filter_by(id=resort_id).one()
 
 
 @router.get("/resorts/{resort_id}", response_model=schemas.Resort)
 def get_resort_by_id(resort_id: str, db_session: Session = Depends(get_api_db)):
+    """Return a single resort"""
     return db_session.query(Resort).filter_by(id=resort_id).one()
 
 
 @router.get("/resorts/{resort_id}/lifts", response_model=List[schemas.Lift])
 def get_lifts_by_resort(resort_id: str, db_session: Session = Depends(get_api_db)):
+    """Return all lifts for a given resort"""
     return (
         db_session.query(Lift)
         .filter_by(resort_id=resort_id)
@@ -154,6 +39,7 @@ def get_lifts_by_resort(resort_id: str, db_session: Session = Depends(get_api_db
 
 @router.get("/resorts/{resort_id}/trails", response_model=List[schemas.Trail])
 def get_trails_by_resort(resort_id: str, db_session: Session = Depends(get_api_db)):
+    """Return all trails for a given resort"""
     return (
         db_session.query(Trail)
         .filter_by(resort_id=resort_id)
--- app.py
+++ app.py
@@ -1,25 +1,13 @@
 """
 FastAPI root
 """
-from datetime import datetime, timedelta, timezone
-import hashlib
-
 from dotenv import dotenv_values
 
-from fastapi import FastAPI, Depends, Response
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.security import OAuth2PasswordRequestForm
 
-from nanoid import generate as generate_id
+from api.endpoints import router
 
-from sqlalchemy.orm import Session
-from sqlalchemy.exc import IntegrityError
-
-from api.endpoints import authorize_new_user, router
-from lib.auth import OTRAuth, JWT, HASH_METHOD
-from lib.models import User
-from lib.postgres import get_api_db
-import lib.schemas as schemas
 
 config = dotenv_values()
 
@@ -44,57 +32,6 @@ origins = [
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    allow_credentials=True,
     allow_headers=["*"],
-    allow_methods=["GET", "POST", "DELETE"],
+    allow_methods=["GET"],
 )
-
-
-@app.post(
-    "/users", dependencies=[Depends(authorize_new_user)], response_model=schemas.User
-)
-async def create_user(
-    new_user: schemas.NewUserRequest, db_session: Session = Depends(get_api_db)
-):
-    """
-    Handle requests for new user registration
-    """
-    hashed_password = hashlib.new(HASH_METHOD, new_user.password.encode()).hexdigest()
-    try:
-        db_session.add(
-            User(
-                id=generate_id(),
-                created_at=datetime.utcnow(),
-                email=new_user.email,
-                email_verified=False,
-                hashed_password=hashed_password,
-            )
-        )
-        db_session.commit()
-
-    except IntegrityError:
-        OTRAuth.return_status(400, detail="Email address already registered")
-
-
-@app.post("/login", response_model=schemas.Token)
-async def login(
-    response: Response,
-    db_session: Session = Depends(get_api_db),
-    form_data: OAuth2PasswordRequestForm = Depends(),
-):
-    """
-    Handle requests to login. Issue a new JWT upon success.
-    """
-    user_email = form_data.username
-    password = form_data.password
-    user = OTRAuth(db_session).authenticate_user(user_email, password)
-    new_jwt = JWT()
-    expires_at = round(
-        (
-            datetime.now(tz=timezone.utc)
-            + timedelta(seconds=int(config.get("TOKEN_EXP_SECONDS", 3600)))
-        ).timestamp()
-    )
-    token = new_jwt.create_token({"sub": user.id, "exp": expires_at})
-    response.headers["Set-Cookie"] = f"otr_auth={token}; HttpOnly; Max-Age=3600"
-    return {"access_token": token, "token_type": "bearer", "expires_at": expires_at}
